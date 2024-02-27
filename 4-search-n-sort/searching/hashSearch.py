7dc602feacee182e21bd8b44c78c1f14a0a5632e
Mugunthan Raju <mugunthanraju2000@gmail.com>
2024-02-27 23:26:47 +0530

    Brush up hash search

---
+++ 4-search-n-sort/searching/hashSearch.py
@@ -0,0 +1,12 @@
+"""
+Hash Search is basically making use of Hash Table (Dictionary in Python)
+"""
+
+sample = dict()
+
+if __name__ == '__main__':
+    sample['one'] = 1 # o(1)
+    sample['two'] = 2
+    sample['three'] = 3
+
+    print(sample['two']) # O(1)
