af7bc57e281323a401a361caae3020e9363ba8ac
Mugunthan Raju <mugunthanraju2000@gmail.com>
2024-02-28 20:01:54 +0530

    Some more hash search

--- 4-search-n-sort/searching/hashSearch.py
+++ 4-search-n-sort/searching/hashSearch.py
@@ -1,12 +1,38 @@
+# hash_table = [None] * 11
+# def h(item): return item % 11
+# def puts(item): hash_table[h(item)] = item
+# def contains(item): return hash_table[h(item)] == item
+# ************************************************************
+
 """
 Hash Search is basically making use of Hash Table (Dictionary in Python)
 """
+# sample = dict()
+# ************************************************************
 
-sample = dict()
+def hash_strings(word):
+    total = 0
+    for c in word:
+        total += ord(c)
+    return total
+# ************************************************************
 
 if __name__ == '__main__':
-    sample['one'] = 1 # o(1)
-    sample['two'] = 2
-    sample['three'] = 3
+    
+    # puts(13)
+    # puts(43)
+    # puts(30)
+    # puts(88)
+    # print(hash_table)
+    # print("contains 77?", contains(77))
+    # print("contains 88?", contains(88))
+# ************************************************************
+    
+    # sample['one'] = 1 # o(1)
+    # sample['two'] = 2
+    # sample['three'] = 3
+    # print(sample['two']) # O(1)
+# ************************************************************
 
-    print(sample['two']) # O(1)
+    print(hash_strings('apple'))
+# ************************************************************
